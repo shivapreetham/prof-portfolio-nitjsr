05215133a302932873b76f2cd70a6b8b443ba17e
shivapreetham <2005shivapreetham@gmail.com>
2025-10-11 13:59:46 +0530

    iframe issue resolved

--- src/app/admin/components/MobilePreview.jsx
+++ src/app/admin/components/MobilePreview.jsx
@@ -1,3 +1,5 @@
+'use client';
+
 import React from "react";
 
 const MobilePreview = () => {
@@ -7,11 +9,10 @@ const MobilePreview = () => {
       <div className="border-[13px] min-w-[340px] w-full max-w-[400px] max-h-[650px] border-black h-screen rounded-[40px] m-2 shadow-md shadow-primary overflow-hidden">
         <iframe
           title="Mobile Preview"
-          src={process.env.NEXT_PUBLIC_BASE_URL}
+          src="/"
           width="100%"
           height="100%"
           className="rounded-[40px]"
-          // scrolling="no"
           style={{ border: "none" }}
         />
       </div>
